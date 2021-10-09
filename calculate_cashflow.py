22e051f06ab69b46c533234656eec15adf1746b8
Braden Richardson <braden.richardson@educationhorizons.com>
2021-10-09 10:48:22 +0800

    introduced start dates, daily spend not working

--- calculate_cashflow.py
+++ calculate_cashflow.py
@@ -13,15 +13,13 @@ import re
 
 ## TODO: Parse decimal dollar values
 ## TODO: Gracefully handle DUE/INCOME events with no dollar value
-## TODO: Calculate daily spending limit
-## TODO: Pass start date through args
-
 
 
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 parser = argparse.ArgumentParser(description="A tool to export UP transactions in a CSV file")
-parser.add_argument('--date', type=str)
+parser.add_argument('--end_date', type=str)
+parser.add_argument('--start_date', type=str)
 parser.add_argument('--coh', type=str)
 args = parser.parse_args()
 
@@ -30,7 +28,7 @@ def main():
     income = 0
     due = 0.0
     try:
-        dtdate = datetime.datetime.strptime(args.date, '%Y-%m-%d')
+        dtdate = datetime.datetime.strptime(args.end_date, '%Y-%m-%d')
         creds = None
         if os.path.exists('token.json'):
             creds = Credentials.from_authorized_user_file('token.json', SCOPES)
@@ -46,7 +44,11 @@ def main():
 
         service = build('calendar', 'v3', credentials=creds)
 
-        now = datetime.datetime.utcnow().isoformat() + 'Z'
+        if args.start_date:
+            startdate = datetime.datetime.strptime(args.start_date, '%Y-%m-%d')
+            now = startdate.isoformat() + 'Z'
+        else:
+            now = datetime.datetime.utcnow().isoformat() + 'Z'
         events_result = service.events().list(calendarId='primary', timeMin=now,
                                             maxResults=150, singleEvents=True,
                                             orderBy='startTime').execute()
@@ -67,11 +69,25 @@ def main():
                     value = re.findall('[0-9]+', event['summary'])
                     due = due + int(value[0])
         
+        delta = date_start - dtdate
+        days = delta.days
+
         cashflow = income - due
         if args.coh:
             cashflow = cashflow + int(args.coh)
+        print(days)
+        daily_spend = round(cashflow / days)
+        weekly_spend = round(daily_spend * 7)
+        
         print('Bills: ${} \nIncome: ${}'.format(due, income))
         print('Spare Cash: ${}'.format(cashflow))
+        if cashflow > 0:
+            print('Daily Spend: ${}'.format(daily_spend))
+            print('Weekly Spend: ${}'.format(weekly_spend))
+        else:
+            print('You\'re fucking broke dude')
+
+
         
 
     except error as E:
